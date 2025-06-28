462a6723166eae25ba05f70801e0b1677cb98762
Alphan Algül <alphanalgul@gmail.com>
2025-06-28 15:21:34 +0300

    Update string_compare.pl

--- Prolog/string_compare.pl
+++ Prolog/string_compare.pl
@@ -1,3 +1,4 @@
+%Checks if the two given strings are equal or not
 string(S1,S2,S3):-
     string(S1),string(S2),
     ( S1 == S2 -> S3 ="These Strings are equal" ; S3="These strings are not equal").
