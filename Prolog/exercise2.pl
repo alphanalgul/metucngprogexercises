a715158787cf961493aa4d36c80df5287e04f9aa
Alphan Algül <alphanalgul@gmail.com>
2025-06-28 14:59:43 +0300

    Update exercise2.pl

--- Prolog/exercise2.pl
+++ Prolog/exercise2.pl
@@ -1,3 +1,6 @@
+%Returns the first and last names of the employees whose department is the same as the 
+%given department or their salary is smaller than the given salary
+
 employee(193,'Jones','John','173 Elm St.','Hoboken','NJ', 12345,1,'25 Jun 93',25500).
 employee(181,'Doe','Betty','11 Spring St.','Paterson','NJ', 12354,3,'12 May 91',28500).
 employee(127,'Jack','Williams','13 Stuart St.','Kingston','NJ', 12445,3,'27 Sep
