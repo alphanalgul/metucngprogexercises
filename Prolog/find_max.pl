4528d51c0ade3a7049e7a237678e16a6996cbbc6
Alphan Algül <alphanalgul@gmail.com>
2025-06-28 15:04:41 +0300

    Update find_max.pl

--- Prolog/find_max.pl
+++ Prolog/find_max.pl
@@ -1,4 +1,4 @@
-% Determines which of the two given numbers is greater
+% Determines which of the two given numbers is greater and prints out the maximum number to the user
 
 find_max(X,Y,Max):-
    ( X>=Y ->Max = X; Max = Y).
