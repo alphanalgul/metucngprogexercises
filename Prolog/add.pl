3338116ec062a9e6d934e2164d389179955badae
Alphan Algül <alphanalgul@gmail.com>
2025-06-28 14:50:12 +0300

    Update add.pl

--- Prolog/add.pl
+++ Prolog/add.pl
@@ -1,3 +1,6 @@
+% add(X, Y, Z) is true when X + Y = Z.
+% Works in any direction if two values are known.
+
 add(X,Y,Z):-number(X),number(Y),Z is X+Y.
 add(X,Y,Z):-number(X),number(Z),Y is Z-X.
 add(X,Y,Z):-number(Y),number(Z),X is Z-Y.
