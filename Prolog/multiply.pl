3b806a956eae1d85102bdf2e5e10b99b10512a80
Alphan Algül <alphanalgul@gmail.com>
2025-06-28 15:07:38 +0300

    Update multiply.pl

--- Prolog/multiply.pl
+++ Prolog/multiply.pl
@@ -1,3 +1,6 @@
+% multiply(X, Y, Z) is true when X * Y = Z.
+% Works in any direction if two values are known.
+
 multiply(X,Y,Z):-number(X),number(Y),Z is X*Y.
 multiply(X,Y,Z):-number(X),number(Z),Y is Z/X.
 multiply(X,Y,Z):-number(Y),number(Z),X is Z/Y.
