9eac15e57e5101e4aa2e790a4ed781e313b0a852
Alphan Algül <alphanalgul@gmail.com>
2025-06-28 14:57:09 +0300

    Update employee.pl

--- Prolog/employee.pl
+++ Prolog/employee.pl
@@ -1,3 +1,5 @@
+%Returns the first name and last name of the employees whose salary is greater than 28 000
+
 employee(193, 'Jones', 'John', '173 Elm St.', 'Hoboken', 'NJ', 12345, 1, '25 Jun 93', 25500).
 employee(181, 'Doe', 'Betty', '11 Spring St.', 'Paterson', 'NJ', 12354, 3, '12 May 91', 28500).
 well_paid_emp(First, Last) :-
