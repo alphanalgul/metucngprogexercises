b91725e6d2325a35fc60b9fe0d60e06ab34ed218
Alphan Algül <alphanalgul@gmail.com>
2025-06-28 15:22:21 +0300

    Update subtract.pl

--- Prolog/subtract.pl
+++ Prolog/subtract.pl
@@ -1,3 +1,6 @@
+% subtract(X, Y, Z) is true when X - Y = Z.
+% Works in any direction if two values are known.
+
 subtract(X,Y,Z):-number(X),number(Y),Z is X-Y.
 subtract(X,Y,Z):-number(X),number(Z), Y is X-Z.
 subtract(X,Y,Z):-number(Y),number(Z), X is Y+Z.
