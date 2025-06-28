906723ef1c84752f99e3712e874d42b4e97d9d9f
Alphan Algül <alphanalgul@gmail.com>
2025-06-28 14:55:20 +0300

    Update divide.pl

--- Prolog/divide.pl
+++ Prolog/divide.pl
@@ -1,3 +1,6 @@
+% divide(X, Y, Z) is true when X / Y = Z.
+% Works in any direction if two values are known.
+
 divide(X,Y,Z):-number(X),number(Y),Z is X/Y.
 divide(X,Y,Z):-number(X),number(Z),Y is X/Z.
 divide(X,Y,Z):-number(Y),number(Z),X is Y*Z.
