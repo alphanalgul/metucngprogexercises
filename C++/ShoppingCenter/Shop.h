46fcc2751200311f6c23c3f5017857af7e96da6f
Alphan Algül <alphanalgul@gmail.com>
2025-06-29 12:58:23 +0300

    Update Shop.h

--- C++/ShoppingCenter/Shop.h
+++ C++/ShoppingCenter/Shop.h
@@ -1,6 +1,4 @@
-/* Alphan Algül 2584639
-I read and accept the submission rules and the extra rules specified
-in each question. This is my own work that is done by myself only */
+
 #ifndef INC_2584639_SHOP_H
 #define INC_2584639_SHOP_H
 #include "Business.h"
