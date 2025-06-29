db3be59fb21755404e1f43b217a65eb05d18c353
Alphan Algül <alphanalgul@gmail.com>
2025-06-29 12:57:57 +0300

    Update Restaurant.cpp

--- C++/ShoppingCenter/Restaurant.cpp
+++ C++/ShoppingCenter/Restaurant.cpp
@@ -1,6 +1,4 @@
-/* Alphan Algül 2584639
-I read and accept the submission rules and the extra rules specified
-in each question. This is my own work that is done by myself only */
+
 #include<iostream>
 #include "Restaurant.h"
 #include<cstring>
